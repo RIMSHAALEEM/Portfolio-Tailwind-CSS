cfab5c938b213b3275bb7dda42fe4cef74a39cc6
RIMSHAALEEM <92934551+RIMSHAALEEM@users.noreply.github.com>
2024-11-10 16:02:43 +0500

    Update page.tsx

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from "@/app/components/Navbar"
-import Footer from '../components/Footer'
-
+import Footer from '@/app/components/Footer'
 const ContactUs = () => {
   return (
     <div>
